4cbaeb3869468fcf4b5fde53703c704d11615a39
kaspervh <kaspervhauschildt@protonmail.com>
2022-11-16 14:53:53 +0100

    made general styling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import FactionsReducer from './FactionsReducer'
 import UnitsReducer from './unitsReducer';
+import PickedUnitsReducer from './PickedUnitsReducer';
 
 const allReducers = combineReducers({
    FactionsReducer,
-   UnitsReducer
+   UnitsReducer,
+   PickedUnitsReducer
 });
 
 export default allReducers
